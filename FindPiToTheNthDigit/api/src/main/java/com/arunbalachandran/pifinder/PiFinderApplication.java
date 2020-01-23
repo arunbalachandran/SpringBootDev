3f5771f4602a7cad7bf02eab87f5b6d6e19e4e48
Arun Balachandran <balachandran.arun94@gmail.com>
2020-01-23 08:43:34 -0600

    Fixed issues with rest controller

--- FindPiToTheNthDigit/api/src/main/java/com/arunbalachandran/pifinder/PiFinderApplication.java
+++ FindPiToTheNthDigit/api/src/main/java/com/arunbalachandran/pifinder/PiFinderApplication.java
@@ -1,10 +1,8 @@
 package com.arunbalachandran.pifinder;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
 
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class PiFinderApplication {
 
     public static void main(String[] args) {
